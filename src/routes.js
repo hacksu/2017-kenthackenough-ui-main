d14f6ceb5f9d1f64def0e1827bf1e1cf52a48779
gabdullah <gabdull2@kent.edu>
2017-08-01 23:13:00 -0400

    Adding info page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Home from 'components/Home/home';
 import Application from 'components/Application/application';
 import NotFound from 'components/NotFound/notFound';
+import EventInfo from 'components/EventInfo/eventInfo';
 
 const routes = [
   {
@@ -11,6 +12,10 @@ const routes = [
   {
     path: '/apply',
     component: Application
+  },
+  {
+    path: '/info',
+    component: EventInfo
   },
   // {
   //   path: '/oauth/callback',
