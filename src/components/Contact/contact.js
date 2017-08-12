bdc4732e4f5e3964056c79ad5b92a6647a0dc012
NickCrawford <ncrawfo7@kent.edu>
2017-08-12 02:53:15 -0400

    Adds contact form API endpoints. Resolves 38

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import axios from 'axios';
 
 import template from './contact.html';
 
 import './contact.scss';
+
+import { API_BASE } from 'src/config/constants';
 
 export default Vue.extend({
   template,
@@ -12,15 +15,28 @@ export default Vue.extend({
 
   data() {
     return {
-      subject: '',
-      name: '',
-      email: '',
-      body: '',
+      ticket: {
+        subject: '',
+        name: '',
+        repyTo: '',
+        body: '',
+      },
     };
   },
 
   methods: {
-    
+    sendMessage() {
+      return axios.post(`${API_BASE}/tickets`, this.ticket)
+      .then((response) => {
+        console.log('Ticket sent', response.data);
+      })
+      .catch((error) => {
+        // Handle error...
+        console.log('API responded with:', error.response.data);
+
+        throw error;
+      });
+    }
   },
 
   mounted: function() {
