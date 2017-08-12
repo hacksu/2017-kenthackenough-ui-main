bd7ddc12dc4b9fd48853fe1ca0caa2093f0b6f72
NickCrawford <ncrawfo7@kent.edu>
2017-08-12 02:06:44 -0400

    Application mobile fixes

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+
+import template from './contact.html';
+
+import './contact.scss';
+import { API_BASE } from 'src/config/constants';
+
+export default Vue.extend({
+  template,
+
+  components: {
+  },
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    
+  },
+
+  mounted: function() {
+
+  }
+
+});
